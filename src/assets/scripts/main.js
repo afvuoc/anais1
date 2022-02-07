977206a6b33b6f5645800d80c5bf8b9e3a11cbd5
AFV UOC <anaisfv.dsn@gmail.com>
2022-02-07 18:34:20 +0100

    m

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -89,3 +89,28 @@ form.addEventListener('submit', function (e) {
     checkEmail(email);
     checkLength(message, 6, 100);
 });
+
+
+$(function() {
+    var letters = $('.jump').text().split('');
+    var spans = '<span>' + letters.join('</span><span>') + '</span>';
+    $('.jump').html(spans);
+
+    var currentSpan = $('.jump span:first');
+
+    setInterval(function() {
+
+      // check if we grabbed the wrong one
+      if(currentSpan.length === 0) {
+        currentSpan = $('.jump span:first');
+      }
+
+      currentSpan.addClass('up').on('transitionend',function(){
+        $(this).removeClass('up');
+      });
+
+      currentSpan = currentSpan.next();
+
+    }, 50);
+
+  });
